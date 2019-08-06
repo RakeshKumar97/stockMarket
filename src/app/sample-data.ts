3b1f63d614978dc35616c71084e19ea3367842eb
RakeshKumar97 <46321721+RakeshKumar97@users.noreply.github.com>
2019-08-06 19:30:52 +0530

    Add files via upload
    i have completed what i know

---
+++ src/app/sample-data.ts
@@ -0,0 +1,11 @@
+import {Stock} from './stock';
+export const STOCKS:Stock[]=[
+  {name:'mphasis', symbol:'NSE', prices :[1010,909] ,  searchTime: "" },
+  {name:'accenture', symbol:'NYSE', prices:[502,503,500] ,searchTime: ""  },
+  {name:'Google', symbol:'NASDAQ', prices:[1997,1998,1999] ,searchTime: ""  },
+    {name:'cognizant', symbol:'NASDAQ', prices:[65,66,67],searchTime: ""   },
+      {name:'microsoft', symbol:'NASDAQ', prices:[136,137,135],searchTime: ""   },
+        {name:'amazon', symbol:'NASDAQ', prices:[1866,1867,1869],searchTime: ""  },
+          {name:'petrol', symbol:'NASDAQ', prices:[80,80,80] ,searchTime: "" },
+            {name:'RR Groups', symbol:'NASDAQ', prices:[40,40,42] ,searchTime: "" },
+]
